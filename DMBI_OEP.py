62b7db67e14680b59b5c00d0235cc77253964a53
sahilshingala00 <56167517+sahilshingala00@users.noreply.github.com>
2020-10-26 12:02:23 +0530

    Add files via upload

---
+++ DMBI_OEP.py
@@ -0,0 +1,54 @@
+
+
+
+# Importing the libraries
+import numpy as np
+#import matplotlib.pyplot as plt
+import pandas as pd
+
+# Part 1 - Data Preprocessing
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
+
+# Encoding categorical data
+# Label Encoding the "Gender" column
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+print(X)
+# One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+print(X)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+# Training the Random Forest Classification model on the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 1000, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+#Prediction
+y_pred = classifier.predict(X_test)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
